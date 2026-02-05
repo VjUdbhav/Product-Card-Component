c766f616571bea411710ce037e939124dd25fb36
VjUdbhav <vjudbhav@gmail.com>
2026-02-05 10:05:05 +0530

    Debugging 2 for vercel

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter basename="/product">
-    <App />
-  </BrowserRouter>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
 
 
 // If you want to start measuring performance in your app, pass a function
