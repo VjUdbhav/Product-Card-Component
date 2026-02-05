927878c41f1cafeecf783f9bd5a704748e1c3a2a
VjUdbhav <vjudbhav@gmail.com>
2026-02-05 10:27:23 +0530

    Debugging 5 for vercel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import ProductCard from "./components/ProductCard";
-import "./App.css";
-
 function App() {
   return (
-    <div className="app">
-      <ProductCard
-        name="Wireless Mouse"
-        price={799}
-        image="https://via.placeholder.com/250"
-        inStock={true}
-      />
-
-      <ProductCard
-        name="Keyboard"
-        price={1999}
-        image="https://via.placeholder.com/250"
-        inStock={false}
-      />
+    <div style={{ padding: "50px", fontSize: "40px" }}>
+      React is Rendering
     </div>
   );
 }
 
 export default App;
+
